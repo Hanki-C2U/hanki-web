4c19b404a1d148b60126acc6fb1a4cb8a0589f9e
ruran8wa <p.rurangwa@alustudent.com>
2025-08-27 12:21:13 +0200

    added whoItsFor component and rendered it in LandingPage as WhoItsFor section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import WhoItsFor from "../components/WhoItsFor";
 
 export default function LandingPage() {
   return (
     <>
     <Header/>
     <Hero/>
+    <WhoItsFor />
     </>
   )
 }
