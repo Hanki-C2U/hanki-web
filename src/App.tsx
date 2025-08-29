c204aabc4dd795cba845098490a7be167a9df97a
bienvenudev <cbienvenu007@gmail.com>
2025-08-29 17:13:35 +0200

    Add routing for ResourceLibrary and MentorDiscovery components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import MentorDashboard from "./pages/MentorDashboard";
 import MenteeDashboard from "./pages/MenteeDashboard";
+import ResourceLibrary from "./pages/ResourceLibrary";
+import MentorDiscovery from "./pages/MentorDiscovery";
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
     <Route path="/" element={<LandingPage/>} />
     <Route path="/mentor-dashboard" element={<MentorDashboard/>} />
     <Route path="/mentee-dashboard" element={<MenteeDashboard/>} />
+    <Route path="/resources" element={<ResourceLibrary/>} />
+    <Route path="/discover-mentors" element={<MentorDiscovery/>} />
    </Routes>
    </BrowserRouter>
   )
