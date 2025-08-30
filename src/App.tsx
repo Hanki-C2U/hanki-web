bd4eb72a2585cfde6edfb62fba862086ac1508e7
bienvenudev <cbienvenu007@gmail.com>
2025-08-30 12:25:38 +0200

    feat: Add BookSession, ProgressTracking, and NotFound components and their routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import MenteeDashboard from "./pages/MenteeDashboard";
 import ResourceLibrary from "./pages/ResourceLibrary";
 import MentorDiscovery from "./pages/MentorDiscovery";
 import MentorProfile from "./pages/MentorProfile";
+import BookSession from "./pages/BookSession";
+import ProgressTracking from "./pages/ProgressTracking";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<LandingPage/>} />
-    <Route path="/mentor-dashboard" element={<MentorDashboard/>} />
-    <Route path="/mentee-dashboard" element={<MenteeDashboard/>} />
-    <Route path="/resources" element={<ResourceLibrary/>} />
-    <Route path="/discover-mentors" element={<MentorDiscovery/>} />
-    <Route path="/mentor/:id" element={<MentorProfile/>} />
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/mentor-dashboard" element={<MentorDashboard />} />
+        <Route path="/mentee-dashboard" element={<MenteeDashboard />} />
+        <Route path="/resources" element={<ResourceLibrary />} />
+        <Route path="/discover-mentors" element={<MentorDiscovery />} />
+        <Route path="/mentor/:id" element={<MentorProfile />} />
+        <Route path="/book-session/:mentorId" element={<BookSession />} />
+        <Route path="/progress" element={<ProgressTracking />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
