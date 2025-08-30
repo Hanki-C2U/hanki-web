e503336af1fa7331dfeda6c048bc7f23e664c5ff
bienvenudev <cbienvenu007@gmail.com>
2025-08-30 23:36:38 +0200

    feat: refactor routing structure by consolidating routes in App component and removing RouteLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import MentorDashboard from "./pages/MentorDashboard";
 import MenteeDashboard from "./pages/MenteeDashboard";
@@ -8,23 +8,34 @@ import MentorProfile from "./pages/MentorProfile";
 import BookSession from "./pages/BookSession";
 import ProgressTracking from "./pages/ProgressTracking";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import AuthCallback from "./pages/AuthCallback";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/mentor-dashboard" element={<MentorDashboard />} />
-        <Route path="/mentee-dashboard" element={<MenteeDashboard />} />
-        <Route path="/resources" element={<ResourceLibrary />} />
-        <Route path="/discover-mentors" element={<MentorDiscovery />} />
-        <Route path="/mentor/:id" element={<MentorProfile />} />
-        <Route path="/book-session/:mentorId" element={<BookSession />} />
-        <Route path="/progress" element={<ProgressTracking />} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<LandingPage />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route path="auth/callback" element={<AuthCallback />} />
+        <Route path="onboarding" element={<Onboarding />} />
+        <Route path="home" element={<h1>Home Page</h1>} />
+        <Route path="mentor-dashboard" element={<MentorDashboard />} />
+        <Route path="mentee-dashboard" element={<MenteeDashboard />} />
+        <Route path="resources" element={<ResourceLibrary />} />
+        <Route path="discover-mentors" element={<MentorDiscovery />} />
+        <Route path="mentor/:id" element={<MentorProfile />} />
+        <Route path="book-session/:mentorId" element={<BookSession />} />
+        <Route path="progress" element={<ProgressTracking />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
