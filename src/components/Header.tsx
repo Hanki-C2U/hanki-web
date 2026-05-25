83bea40d0cd66684be2473eb567ece0497253449
bienvenudev <cbienvenu007@gmail.com>
2026-05-25 18:04:12 +0200

    fix: remove unused Link import from Header component and update tsconfig to allow unused locals

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { startDemoSession } from "../lib/demoSession";
 
 export default function Header() {
